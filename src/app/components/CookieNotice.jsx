43505cdc5a231ee612b48d8da6a48f4cd5d5eda1
Zeeshan <mzeeshi187@gmail.com>
2025-09-12 04:09:10 -0700

    linking done

--- src/app/components/CookieNotice.jsx
+++ src/app/components/CookieNotice.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import { LiaTimesSolid } from "react-icons/lia";
 
 export default function CookieNotice() {
   const [visible, setVisible] = useState(false);
@@ -43,13 +44,17 @@ export default function CookieNotice() {
     setVisible(false);
   };
 
+  const handleClose = () => {
+    setVisible(false); // Hide without saving to localStorage
+  };
+
   if (!visible) return null;
 
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-[#140032] text-white px-6 py-4 flex items-center justify-between z-50 shadow-lg">
-      <div>
-        <p className="font-medium text-sm">Cookie Notice</p>
-        <p className="text-xs text-gray-300">
+    <div className="fixed bottom-0 h-[88px] left-0 right-0 bg-[#140032] text-white px-6 md:px-[100px] py-4 flex items-center justify-between z-50 shadow-lg">
+      <div className="w-[40%] sm:w-auto">
+        <p className="font-bold text-[10px] sm:text-lg">Cookie Notice</p>
+        <p className="text-[8px] sm:text-base text-white">
           We use cookies to improve your experience. Essential cookies will
           always be enabled for the site to function properly.
         </p>
@@ -57,17 +62,24 @@ export default function CookieNotice() {
       <div className="flex items-center gap-2">
         <button
           onClick={handleReject}
-          className="px-4 py-1 border border-gray-400 rounded-full text-sm hover:bg-gray-700 transition"
+          className="w-[70px] sm:w-[113px] h-[40px] border border-gray-400 rounded-full text-sm hover:bg-gray-700 transition"
         >
           Reject
         </button>
         <button
           onClick={handleAccept}
-          className="px-4 py-1 bg-white text-black rounded-full text-sm hover:bg-gray-200 transition"
+          className="w-[70px] mr-[20px] sm:mr-0 sm:w-[113px] h-[40px] bg-white text-black rounded-full text-sm hover:bg-gray-200 transition"
         >
           Accept
         </button>
       </div>
+      <button
+        onClick={handleClose}
+        className="absolute top-[15px] right-[15px] bg-white/20 w-[24px] h-[24px] flex justify-center items-center rounded-full text-white hover:text-gray-300 transition"
+        aria-label="Close cookie notice"
+      >
+        <LiaTimesSolid size={15} />
+      </button>
     </div>
   );
 }
